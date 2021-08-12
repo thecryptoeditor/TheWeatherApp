73b7510a7c61e4f18ab5a5213b2327618d1e674d
Admin <jatin.aggarwal.15csc@bml.edu.in>
2021-08-13 00:56:10 +0530

    Various style related changes done.

--- src/components/TechNews.js
+++ src/components/TechNews.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import '../Assets/common.css'
 
 export const TechNews = () => {
 
-	const [tech, techData] = React.useState([]);
+	const [tech, techData] = React.useState({
+		dataArray: []
+	});
 
 	const articleData = async () => {
         const result = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6be551b32ce44eb88189a07bdcd5c324');
         let res = await result.json();
-        techData(res.articles)
+        techData({dataArray: res.articles})
     };
 
 	//tech new related API call: 'https://newsapi.org/'
@@ -15,13 +18,28 @@ export const TechNews = () => {
     	articleData();
     }, [])
 
-    console.log(tech)
-
 	return (
 		<div className="text-center mt-20 container">
-{/*			<img alt="tect_img" src={tech.articleData[0].urlToImage}/>
-			<h5>{tech.articleData[0].title}</h5>
-			<p>{tech.articleData[0].description}</p>*/}
+			{tech.dataArray.map(i => (
+				<div key={i.title} className="mobile_view_width" style={{width: "60%", margin: "auto", paddingTop: "50px"}}>
+					<h3 style={{textAlign: "left"}}>{i.title}</h3>
+					<span style={{marginTop: "20px"}} className="d-flex justify-content-between">
+						<p style={{margin: "0px 0px 5px 0px"}}>Published By: {i.author}</p>
+						<p style={{margin: "0px 0px 5px 0px"}}>
+							{new Intl.DateTimeFormat('en-GB', { 
+				        		month: 'long', 
+				            	day: '2-digit',
+				            	year: 'numeric', 
+				        	}).format(new Date(i.publishedAt))}
+				        </p>
+					</span>
+					<img style={{margin: "0px 0px 30px 0px", width: "100%"}} alt="tect_img" src={i.urlToImage}/>
+					<p style={{textAlign: "left"}}>{i.description} <a style={{textDecoration: "underline", color: "black", fontStyle: "italic"}} href={i.url}>continue</a></p>
+					<span className="date timeago" title={ i.publishedAt }>
+
+				    </span>
+				</div>
+			))}
 		</div>
 	)
 
